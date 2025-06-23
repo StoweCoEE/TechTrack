49b0ed53b02bfd587282880c9bbdfa472e91cd2d
stoh <stowecounsell@gmail.com>
2025-06-23 15:45:34 -0400

    Added Logo to top of main screen

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sqlite3
 
 # Open tkinter GUI window and set dimensions and title
 root = Tk()
-root.geometry("1280x720")
+root.geometry('1280x720')
 root.title("TechTrack")
 
 # Set GUI window icon and background color
@@ -13,11 +13,11 @@ icon = PhotoImage(file='images\\techtrack_logo.png')
 root.iconphoto(True, icon)
 
 # Creates graphic with name and logo
-logo = PhotoImage(file='images\\techtrack_full.png')
-logocanvas = Canvas(root, bg="#bcdfeb")
-logocanvas.pack()
-logocanvas.create_image(0,0,image=logo)
-#testing to see if you see anything
+tt_logo = Image.open('images\\techtrack_full.png').resize((394,197))
+tt_logo_tk = ImageTk.PhotoImage(tt_logo)
+logocanvas = Canvas(root, bg="#bcdfeb", bd=0, highlightthickness=0, width=394, height=197)
+logocanvas.pack(pady=15)
+logocanvas.create_image(0, 0, image=tt_logo_tk, anchor=NW)
 
 # tkinter GUI loop runs application
 root.mainloop()
