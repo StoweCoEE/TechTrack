4de080b706fed79c83013774072d37c3258c9c83
stoh <stowecounsell@gmail.com>
2025-06-23 18:14:09 -0400

    Home Page & Navigation Functional

--- main.py
+++ main.py
@@ -2,29 +2,6 @@ from tkinter import *
 from PIL import ImageTk, Image
 import sqlite3
 
-"""
-# Open tkinter GUI window and set dimensions and title
-root = Tk()
-root.geometry('1280x720')
-root.title("TechTrack")
-
-# Set GUI window icon and background color
-root.config(background="#bcdfeb")
-icon = PhotoImage(file='images\\techtrack_logo.png')
-root.iconphoto(True, icon)
-
-# Creates graphic with name and logo
-tt_logo = Image.open('images\\techtrack_full.png').resize((394,197))
-tt_logo_tk = ImageTk.PhotoImage(tt_logo)
-logocanvas = Canvas(root, bg="#bcdfeb", bd=0, highlightthickness=0, width=394, height=197)
-logocanvas.pack(pady=15)
-logocanvas.create_image(0, 0, image=tt_logo_tk, anchor=NW)
-
-# tkinter GUI loop runs application
-root.mainloop()
-"""
-
-
 class TechTrack(Tk):
     def __init__(self):
         super().__init__()
@@ -50,14 +27,21 @@ class HomePage(Frame):
         super().__init__(parent)
         
         self.create_logo()
-        Label(self, text="This is the start page").pack(pady=10)
+        Label(self, text="This is the start page").pack()
         Button(self, text="Add Asset",
                command=lambda: parent.switch_frame(AddAssetPage)).pack()
         Button(self, text="Add Worksite",
                command=lambda: parent.switch_frame(AddWorksitePage)).pack()
+        Button(self, text="Update Asset/Worksite",
+               command=lambda: parent.switch_frame(UpdatePage)).pack()
+        Button(self, text="Delete Asset/Worksite",
+               command=lambda: parent.switch_frame(DeletePage)).pack()
+        Button(self, text="View Assets/Worksites",
+               command=lambda: parent.switch_frame(ViewPage)).pack()
         
         
     def create_logo(self):
+        global tt_logo_tk
         tt_logo = Image.open('images\\techtrack_full.png').resize((394,197))
         tt_logo_tk = ImageTk.PhotoImage(tt_logo)
         logocanvas = Canvas(self, bg="#bcdfeb", bd=0, highlightthickness=0, width=394, height=197)
@@ -75,6 +59,27 @@ class AddWorksitePage(Frame):
     def __init__(self, parent):
         super().__init__(parent)
         Label(self, text="Add Worksite Page").pack(side="top", fill="x", pady=10)
+        Button(self, text="Return to Home Page",
+               command=lambda: parent.switch_frame(HomePage)).pack()
+        
+class UpdatePage(Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        Label(self, text="Update Page").pack(side="top", fill="x", pady=10)
+        Button(self, text="Return to Home Page",
+               command=lambda: parent.switch_frame(HomePage)).pack()
+        
+class DeletePage(Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        Label(self, text="Delete Page").pack(side="top", fill="x", pady=10)
+        Button(self, text="Return to Home Page",
+               command=lambda: parent.switch_frame(HomePage)).pack()
+        
+class ViewPage(Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        Label(self, text="View Page").pack(side="top", fill="x", pady=10)
         Button(self, text="Return to Home Page",
                command=lambda: parent.switch_frame(HomePage)).pack()
         
