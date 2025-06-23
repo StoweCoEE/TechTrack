22839dddb91e273e4ff77d1aed795cbc3c6558bb
stoh <stowecounsell@gmail.com>
2025-06-23 13:24:54 -0400

    Added images folder

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ window.geometry("1280x720")
 window.title("TechTrack")
 
 window.config(background="#bcdfeb")
-icon = PhotoImage(file="techtrack_logo.png")
+icon = PhotoImage(file='images\\techtrack_logo.png')
 window.iconphoto(True, icon)
+
+logo = PhotoImage(file='images\\techtrack_full.png')
+logocanvas = Canvas(window, bg="#bcdfeb")
+logocanvas.pack()
+
+logocanvas.create_image(0,0,image=logo)
 
 window.mainloop()
