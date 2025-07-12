5b2620ae902c831c53eaf36c793605b1ea585292
stoh <stowecounsell@gmail.com>
2025-07-12 16:12:23 -0400

    Finished Initial Layout Setup

--- main.py
+++ main.py
@@ -3,6 +3,27 @@ from PIL import ImageTk, Image
 import sqlite3
 
 
+##
+## SQLITE DATABASE INITIATION (TO BE ADDED LATER)
+##
+
+#dataBase = sqlite3.connect('TechTrackDB')
+#cursor = dataBase.cursor()
+
+
+##
+## WIDGET FUNCTION DEFINITIONS
+##
+
+def EntitySearchClick():
+    print("EntitySearchClick")
+
+def AssetSearchClick():
+    print("AssetSearchClick")
+
+def WorksiteSearchClick():
+    print("WorksiteSearchClick")
+
 class TechTrack(Tk):
     def __init__(self):
         super().__init__()
@@ -122,18 +143,28 @@ class AddAssetPage(Frame):
         # Create and Place Labels and User-Entry Widgets
         Label(contentFrame,text="Asset Name:",font=('Arial',26),bg="#bcdfeb",justify="right",width=20).grid(row=1,column=0)
         userEntry1 = Entry(contentFrame,font=('Arial',20),relief=RIDGE,width=50)
+        userEntry1.insert(0,'(Enter Asset Name)')
+        userEntry1.bind("<FocusIn>", lambda args: userEntry1.delete('0', 'end'))
         userEntry1.grid(row=1,column=1,columnspan=3,sticky=W)
         Label(contentFrame,text="Asset Type:",font=('Arial',26),bg="#bcdfeb",justify="right").grid(row=2,column=0)
         userEntry2 = Entry(contentFrame,font=('Arial',20),relief=RIDGE,width=50)
+        userEntry2.insert(0,'(Enter Asset Type)')
+        userEntry2.bind("<FocusIn>", lambda args: userEntry2.delete('0', 'end'))
         userEntry2.grid(row=2,column=1,columnspan=3,sticky=W)
         Label(contentFrame,text="Asset Model Number:",font=('Arial',26),bg="#bcdfeb",justify="right").grid(row=3,column=0)
         userEntry3 = Entry(contentFrame,font=('Arial',20),relief=RIDGE,width=50)
+        userEntry3.insert(0,'(Enter Asset Model Number)')
+        userEntry3.bind("<FocusIn>", lambda args: userEntry3.delete('0', 'end'))
         userEntry3.grid(row=3,column=1,columnspan=3,sticky=W)
         Label(contentFrame,text="Purchase Date:",font=('Arial',26),bg="#bcdfeb",justify="right").grid(row=4,column=0)
         userEntry4 = Entry(contentFrame,font=('Arial',20),relief=RIDGE,width=50)
+        userEntry4.insert(0,'(Enter Asset Purchase Date)')
+        userEntry4.bind("<FocusIn>", lambda args: userEntry4.delete('0', 'end'))
         userEntry4.grid(row=4,column=1,columnspan=3,sticky=W)
         Label(contentFrame,text="Asset Cost:",font=('Arial',26),bg="#bcdfeb",justify="right").grid(row=5,column=0)
         userEntry5 = Entry(contentFrame,font=('Arial',20),relief=RIDGE,width=50)
+        userEntry5.insert(0,'(Enter Asset Cost)')
+        userEntry5.bind("<FocusIn>", lambda args: userEntry5.delete('0', 'end'))
         userEntry5.grid(row=5,column=1,columnspan=3,sticky=W)
         
         # Button to submit Changes
@@ -187,18 +218,28 @@ class AddWorksitePage(Frame):
         # Create and Place Labels and User-Entry Widgets
         Label(contentFrame,text="Order ID:",font=('Arial',26),bg="#bcdfeb",justify="right",width=20).grid(row=1,column=0)
         userEntry1 = Entry(contentFrame,font=('Arial',20),relief=RIDGE,width=50)
+        userEntry1.insert(0,'(Enter Worksite Name)')
+        userEntry1.bind("<FocusIn>", lambda args: userEntry1.delete('0', 'end'))
         userEntry1.grid(row=1,column=1,columnspan=3,sticky=W)
         Label(contentFrame,text="Worksite Type:",font=('Arial',26),bg="#bcdfeb",justify="right").grid(row=2,column=0)
         userEntry2 = Entry(contentFrame,font=('Arial',20),relief=RIDGE,width=50)
+        userEntry2.insert(0,'(Enter Worksite Type)')
+        userEntry2.bind("<FocusIn>", lambda args: userEntry2.delete('0', 'end'))
         userEntry2.grid(row=2,column=1,columnspan=3,sticky=W)
         Label(contentFrame,text="Worksite Address:",font=('Arial',26),bg="#bcdfeb",justify="right").grid(row=3,column=0)
         userEntry3 = Entry(contentFrame,font=('Arial',20),relief=RIDGE,width=50)
+        userEntry3.insert(0,'(Enter Worksite Address)')
+        userEntry3.bind("<FocusIn>", lambda args: userEntry3.delete('0', 'end'))
         userEntry3.grid(row=3,column=1,columnspan=3,sticky=W)
         Label(contentFrame,text="Worksite City:",font=('Arial',26),bg="#bcdfeb",justify="right").grid(row=4,column=0)
         userEntry4 = Entry(contentFrame,font=('Arial',20),relief=RIDGE,width=50)
+        userEntry4.insert(0,'(Enter Worksite City)')
+        userEntry4.bind("<FocusIn>", lambda args: userEntry4.delete('0', 'end'))
         userEntry4.grid(row=4,column=1,columnspan=3,sticky=W)
         Label(contentFrame,text="Worksite Zip:",font=('Arial',26),bg="#bcdfeb",justify="right").grid(row=5,column=0)
         userEntry5 = Entry(contentFrame,font=('Arial',20),relief=RIDGE,width=50)
+        userEntry5.insert(0,'(Enter Worksite Zip Code)')
+        userEntry5.bind("<FocusIn>", lambda args: userEntry5.delete('0', 'end'))
         userEntry5.grid(row=5,column=1,columnspan=3,sticky=W)
         
         # Button to submit Changes
@@ -217,7 +258,7 @@ class UpdatePage(Frame):
         self.config(background="#bcdfeb")
         # Create frames for header and content
         headerFrame = Frame(self, bg="#9ed1e1")
-        contentFrame = Frame(self, bg="#9ed1e1")
+        contentFrame = Frame(self, bg="#bcdfeb")
         # grid propagate so we can force frame sizes
         headerFrame.grid_propagate(0)
         contentFrame.grid_propagate(0)
@@ -249,6 +290,14 @@ class UpdatePage(Frame):
         Button(headerFrame, text="Return to Home Page",
                command=lambda: parent.switch_frame(HomePage)).pack(side="left",fill="y")
         
+        # Search Bar and Button
+        Label(contentFrame,text="Asset/Worksite ID:",font=('Arial',26),bg="#bcdfeb",justify="right",width=20).grid(row=0,column=0)
+        searchEntry1 = Entry(contentFrame,font=('Arial',20),relief=RIDGE,width=38)
+        searchEntry1.grid(row=0,column=1,columnspan=3,sticky=W)
+        searchBtn = Button(contentFrame, text="Search", font=('Arial',20))
+        searchBtn.config(command=EntitySearchClick)
+        searchBtn.grid(row=0,column=4, sticky=W)
+
 class DeletePage(Frame):
     def __init__(self, parent):
         super().__init__(parent)
@@ -261,7 +310,7 @@ class DeletePage(Frame):
         self.config(background="#bcdfeb")
         # Create frames for header and content
         headerFrame = Frame(self, bg="#9ed1e1")
-        contentFrame = Frame(self, bg="#9ed1e1")
+        contentFrame = Frame(self, bg="#bcdfeb")
         # grid propagate so we can force frame sizes
         headerFrame.grid_propagate(0)
         contentFrame.grid_propagate(0)
@@ -292,6 +341,13 @@ class DeletePage(Frame):
         Label(headerFrame, text="Delete an Entity",font=('Arial',36,'bold'),bg="#9ed1e1",width=33).pack(side="left")
         Button(headerFrame, text="Return to Home Page",
                command=lambda: parent.switch_frame(HomePage)).pack(side="left",fill="y")
+        
+        # Search Entry Bar and Button
+        Label(contentFrame,text="Asset/Worksite ID:",font=('Arial',26),bg="#bcdfeb",justify="right",width=20).grid(row=0,column=0)
+        searchEntry1 = Entry(contentFrame,font=('Arial',20),relief=RIDGE,width=38)
+        searchEntry1.grid(row=0,column=1,columnspan=3,sticky=W)
+        searchBtn = Button(contentFrame, text="Search", font=('Arial',20))
+        searchBtn.grid(row=0,column=4, sticky=W)
         
 class ViewPage(Frame):
     def __init__(self, parent):
@@ -336,6 +392,12 @@ class ViewPage(Frame):
         Label(headerFrame, text="Entity Viewer",font=('Arial',36,'bold'),bg="#9ed1e1",width=33).pack(side="left")
         Button(headerFrame, text="Return to Home Page",
                command=lambda: parent.switch_frame(HomePage)).pack(side="left",fill="y")
+        
+        # Button to View DB info by Assets or Worksites
+        viewAssetBtn = Button(contentFrame, text="View Assets", font=('Arial',20))
+        viewAssetBtn.grid(row=0,column=0,columnspan=3)
+        viewWorksiteBtn = Button(contentFrame, text="View Worksites", font=('Arial',20))
+        viewWorksiteBtn.grid(row=0,column=4,columnspan=3)
         
 if __name__ == "__main__":
     tt = TechTrack()
