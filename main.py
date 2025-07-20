cf40be4f15b40a7c57d0161a10eafc15e5cb4907
stoh <stowecounsell@gmail.com>
2025-07-20 19:17:28 -0400

    Finished Adding functionality to all pages and widgets. Application is now fully functional.

--- main.py
+++ main.py
@@ -330,6 +330,220 @@ class AddWorksitePage(Frame):
 class UpdatePage(Frame):
     def __init__(self, parent):
         Frame.__init__(self, parent)
+
+        def searchOneAsset():
+            assetID = searchEntry1.get()
+
+            if assetID == "":
+                messagebox.showerror("Missing Entry","Please Enter an Entity ID")
+                searchEntry1.delete(0,END)
+            else:
+                foundLabel = Label(resultFrame,text="Asset Found:",font=('Arial',20),bg="#bcdfeb")
+                foundLabel.grid(row=0,column=0,columnspan=7)
+                searchResult=database.viewOneAsset(conn, assetID)
+
+                label0 = Label(resultFrame,text="Asset ID:",font=('Arial',26),bg="#bcdfeb",justify="right",width=20)
+                label0.grid(row=1,column=0,columnspan=3)
+                userEntry0 = Entry(resultFrame,font=('Arial',20),relief=RIDGE,width=50)
+                userEntry0.insert(0,searchResult[0])
+                userEntry0.config(state="readonly")
+                userEntry0.grid(row=1,column=3,columnspan=3,sticky=W)
+
+                label1 = Label(resultFrame,text="Asset Name:",font=('Arial',26),bg="#bcdfeb",justify="right",width=20)
+                label1.grid(row=2,column=0,columnspan=3)
+                userEntry1 = Entry(resultFrame,font=('Arial',20),relief=RIDGE,width=50)
+                userEntry1.insert(0,searchResult[2])
+                userEntry1.grid(row=2,column=3,columnspan=3,sticky=W)
+
+                label2 = Label(resultFrame,text="Asset Type:",font=('Arial',26),bg="#bcdfeb",justify="right")
+                label2.grid(row=3,column=0,columnspan=3)
+                userEntry2 = Entry(resultFrame,font=('Arial',20),relief=RIDGE,width=50)
+                userEntry2.insert(0,searchResult[1])
+                userEntry2.grid(row=3,column=3,columnspan=3,sticky=W)
+
+                label3 = Label(resultFrame,text="Asset Model Number:",font=('Arial',26),bg="#bcdfeb",justify="right")
+                label3.grid(row=4,column=0,columnspan=3)
+                userEntry3 = Entry(resultFrame,font=('Arial',20),relief=RIDGE,width=50)
+                userEntry3.insert(0,searchResult[5])
+                userEntry3.grid(row=4,column=3,columnspan=3,sticky=W)
+
+                label4 = Label(resultFrame,text="Purchase Date (YYYYMMDD):",font=('Arial',26),bg="#bcdfeb",justify="right")
+                label4.grid(row=5,column=0,columnspan=3)
+                userEntry4 = Entry(resultFrame,font=('Arial',20),relief=RIDGE,width=50)
+                userEntry4.insert(0,searchResult[4])
+                userEntry4.grid(row=5,column=3,columnspan=3,sticky=W)
+
+                label5 = Label(resultFrame,text="Asset Cost:",font=('Arial',26),bg="#bcdfeb",justify="right")
+                label5.grid(row=6,column=0,columnspan=3)
+                userEntry5 = Entry(resultFrame,font=('Arial',20),relief=RIDGE,width=50)
+                userEntry5.insert(0,searchResult[3])
+                userEntry5.grid(row=6,column=3,columnspan=3,sticky=W)
+                
+                searchOneAssetBtn.config(state=DISABLED)
+                searchOneWorksiteBtn.config(state=DISABLED)
+
+                confirmAssetBtn = Button(resultFrame, text="Confirm Update", font=('Arial',20),
+                                         command=lambda: UpdateAsset())
+                confirmAssetBtn.grid(row=7,column=0,columnspan=4)
+
+                resetAssetBtn = Button(resultFrame, text="Reset", font=('Arial',20),command=lambda: resetAsset())
+                resetAssetBtn.grid(row=7,column=4,columnspan=4)
+
+            def UpdateAsset():
+                assetID=userEntry0.get()
+                assetType=userEntry2.get()
+                assetName=userEntry1.get()
+                assetCost=userEntry5.get()
+                assetPurchase=userEntry4.get()
+                assetModel=userEntry3.get()
+                database.deleteAsset(conn, assetID)
+                database.updateAsset(conn, assetID,assetType,assetName,assetCost,assetPurchase,assetModel)
+                messagebox.showinfo("Update Success","Asset Updated Successfully")
+                foundLabel.destroy()
+                label0.destroy()
+                label1.destroy()
+                label2.destroy()
+                label3.destroy()
+                label4.destroy()
+                label5.destroy()
+                userEntry0.destroy()
+                userEntry1.destroy()
+                userEntry2.destroy()
+                userEntry3.destroy()
+                userEntry4.destroy()
+                userEntry5.destroy()
+                resetAssetBtn.destroy()
+                confirmAssetBtn.destroy()
+                searchOneAssetBtn.config(state=ACTIVE)
+                searchOneWorksiteBtn.config(state=ACTIVE)
+                searchEntry1.delete(0,END)
+
+            def resetAsset():
+                foundLabel.destroy()
+                label0.destroy()
+                label1.destroy()
+                label2.destroy()
+                label3.destroy()
+                label4.destroy()
+                label5.destroy()
+                userEntry0.destroy()
+                userEntry1.destroy()
+                userEntry2.destroy()
+                userEntry3.destroy()
+                userEntry4.destroy()
+                userEntry5.destroy()
+                resetAssetBtn.destroy()
+                confirmAssetBtn.destroy()
+                searchOneAssetBtn.config(state=ACTIVE)
+                searchOneWorksiteBtn.config(state=ACTIVE)
+                searchEntry1.delete(0,END)
+
+        def searchOneWorksite():
+            worksiteID = searchEntry1.get()
+
+            if worksiteID == "":
+                messagebox.showerror("Missing Entry","Please Enter an Entity ID")
+                searchEntry1.delete(0,END)
+            else:
+                foundLabel = Label(resultFrame,text="Worksite Found:",font=('Arial',20),bg="#bcdfeb")
+                foundLabel.grid(row=0,column=0,columnspan=7)
+                searchResult=database.viewOneWorksite(conn, worksiteID)
+
+                label0 = Label(resultFrame,text="Worksite ID:",font=('Arial',26),bg="#bcdfeb",justify="right",width=20)
+                label0.grid(row=1,column=0,columnspan=3)
+                userEntry0 = Entry(resultFrame,font=('Arial',20),relief=RIDGE,width=50)
+                userEntry0.insert(0,searchResult[0])
+                userEntry0.config(state="readonly")
+                userEntry0.grid(row=1,column=3,columnspan=3,sticky=W)
+
+                label1 = Label(resultFrame,text="Order ID:",font=('Arial',26),bg="#bcdfeb",justify="right",width=20)
+                label1.grid(row=2,column=0,columnspan=3)
+                userEntry1 = Entry(resultFrame,font=('Arial',20),relief=RIDGE,width=50)
+                userEntry1.insert(0,searchResult[1])
+                userEntry1.grid(row=2,column=3,columnspan=3,sticky=W)
+
+                label2 = Label(resultFrame,text="Worksite Type:",font=('Arial',26),bg="#bcdfeb",justify="right")
+                label2.grid(row=3,column=0,columnspan=3)
+                userEntry2 = Entry(resultFrame,font=('Arial',20),relief=RIDGE,width=50)
+                userEntry2.insert(0,searchResult[2])
+                userEntry2.grid(row=3,column=3,columnspan=3,sticky=W)
+
+                label3 = Label(resultFrame,text="Worksite Address:",font=('Arial',26),bg="#bcdfeb",justify="right")
+                label3.grid(row=4,column=0,columnspan=3)
+                userEntry3 = Entry(resultFrame,font=('Arial',20),relief=RIDGE,width=50)
+                userEntry3.insert(0,searchResult[3])
+                userEntry3.grid(row=4,column=3,columnspan=3,sticky=W)
+
+                label4 = Label(resultFrame,text="Worksite City:",font=('Arial',26),bg="#bcdfeb",justify="right")
+                label4.grid(row=5,column=0,columnspan=3)
+                userEntry4 = Entry(resultFrame,font=('Arial',20),relief=RIDGE,width=50)
+                userEntry4.insert(0,searchResult[4])
+                userEntry4.grid(row=5,column=3,columnspan=3,sticky=W)
+
+                label5 = Label(resultFrame,text="Worksite Zip:",font=('Arial',26),bg="#bcdfeb",justify="right")
+                label5.grid(row=6,column=0,columnspan=3)
+                userEntry5 = Entry(resultFrame,font=('Arial',20),relief=RIDGE,width=50)
+                userEntry5.insert(0,searchResult[5])
+                userEntry5.grid(row=6,column=3,columnspan=3,sticky=W)
+                
+                searchOneAssetBtn.config(state=DISABLED)
+                searchOneWorksiteBtn.config(state=DISABLED)
+
+                confirmAssetBtn = Button(resultFrame, text="Confirm Update", font=('Arial',20),
+                                         command=lambda: UpdateWorksite())
+                confirmAssetBtn.grid(row=7,column=0,columnspan=4)
+
+                resetAssetBtn = Button(resultFrame, text="Reset", font=('Arial',20),command=lambda: resetAsset())
+                resetAssetBtn.grid(row=7,column=4,columnspan=4)
+
+            def UpdateWorksite():
+                worksiteID=userEntry0.get()
+                orderID=userEntry1.get()
+                workType=userEntry2.get()
+                address=userEntry3.get()
+                city=userEntry4.get()
+                zip=userEntry5.get()
+                database.deleteWorksite(conn, worksiteID)
+                database.updateWorksite(conn, worksiteID,orderID,workType,address,city,zip)
+                messagebox.showinfo("Update Success","Worksite Updated Successfully")
+                foundLabel.destroy()
+                label0.destroy()
+                label1.destroy()
+                label2.destroy()
+                label3.destroy()
+                label4.destroy()
+                label5.destroy()
+                userEntry0.destroy()
+                userEntry1.destroy()
+                userEntry2.destroy()
+                userEntry3.destroy()
+                userEntry4.destroy()
+                userEntry5.destroy()
+                resetAssetBtn.destroy()
+                confirmAssetBtn.destroy()
+                searchOneAssetBtn.config(state=ACTIVE)
+                searchOneWorksiteBtn.config(state=ACTIVE)
+                searchEntry1.delete(0,END)
+
+            def resetAsset():
+                foundLabel.destroy()
+                label0.destroy()
+                label1.destroy()
+                label2.destroy()
+                label3.destroy()
+                label4.destroy()
+                label5.destroy()
+                userEntry0.destroy()
+                userEntry1.destroy()
+                userEntry2.destroy()
+                userEntry3.destroy()
+                userEntry4.destroy()
+                userEntry5.destroy()
+                resetAssetBtn.destroy()
+                confirmAssetBtn.destroy()
+                searchOneAssetBtn.config(state=ACTIVE)
+                searchOneWorksiteBtn.config(state=ACTIVE)
+                searchEntry1.delete(0,END)
         
         ##============================================================
         ## LAYOUT SETTINGS
@@ -340,18 +554,24 @@ class UpdatePage(Frame):
         # Create frames for header and content
         headerFrame = Frame(self, bg="#9ed1e1")
         contentFrame = Frame(self, bg="#bcdfeb")
+        resultFrame = Frame(self, bg="#bcdfeb")
         # grid propagate so we can force frame sizes
         headerFrame.grid_propagate(0)
         contentFrame.grid_propagate(0)
+        resultFrame.grid_propagate(0)
         # configure frame sizes
         headerFrame.config(width=1220,height=100)
-        contentFrame.config(width=1220,height=600)
+        contentFrame.config(width=1220,height=150)
+        resultFrame.config(width=1220,height=400)
         # pack frames onto main frame
         headerFrame.pack(side="top")
+        resultFrame.pack(side="bottom")
         contentFrame.pack(side="bottom")
         # Establish grid for content frame
-        contentFrame.columnconfigure((0,1,2,3,4,5,6),weight=1)
+        contentFrame.columnconfigure((0,1,2,3,4,5,6),weight=1,minsize=182)
         contentFrame.rowconfigure((0,1,2,3,4,5,6,7),weight=1)
+        resultFrame.columnconfigure((0,1,2,3,4,5,6),weight=1,minsize=182)
+        resultFrame.rowconfigure((0,1,2,3,4,5,6,7),weight=1)
 
         ##============================================================
         ## WIDGET SETTINGS
@@ -371,13 +591,14 @@ class UpdatePage(Frame):
                command=lambda: parent.switch_frame(HomePage)).pack(side="left",fill="y")
         
         # Search Bar and Button
-        Label(contentFrame,text="Asset/Worksite ID:",font=('Arial',26),bg="#bcdfeb",justify="right",width=20).grid(row=0,column=0)
-        searchEntity1 = Entry(contentFrame,font=('Arial',20),relief=RIDGE,width=38)
-        searchEntity1.insert(0,'(Enter Asset/Worksite ID)')
-        searchEntity1.bind("<FocusIn>", lambda args: searchEntity1.delete('0', 'end'))
-        searchEntity1.grid(row=0,column=1,columnspan=3,sticky=W)
-        searchEntityBtn = Button(contentFrame, text="Search", font=('Arial',20))
-        searchEntityBtn.grid(row=0,column=4, sticky=W)
+        Label(contentFrame,text="Asset/Worksite ID:",font=('Arial',20),bg="#bcdfeb",justify="right",width=20,anchor=E
+              ).grid(row=0,column=0, columnspan=3)
+        searchEntry1 = Entry(contentFrame,font=('Arial',20),relief=RIDGE,width=34)
+        searchEntry1.grid(row=0,column=3,columnspan=4,sticky=W)
+        searchOneAssetBtn = Button(contentFrame, text="Search Asset", font=('Arial',20),command=lambda: searchOneAsset())
+        searchOneAssetBtn.grid(row=1,column=1, columnspan=3)
+        searchOneWorksiteBtn = Button(contentFrame, text="Search Worksite", font=('Arial',20),command=lambda: searchOneWorksite())
+        searchOneWorksiteBtn.grid(row=1,column=3, columnspan=3)
 
 class DeletePage(Frame):
     def __init__(self, parent):
@@ -694,9 +915,78 @@ class ViewPage(Frame):
                 messagebox.showerror("Missing Data","No Worksites Found")
                 softResetView()
             
-
         def DisplayAssignments():
-            pass
+            def resetView():
+                viewAssetBtn.config(state=ACTIVE)
+                viewWorksiteBtn.config(state=ACTIVE)
+                viewAssignmentsBtn.config(state=ACTIVE)
+                assignmentTree.destroy()
+                resetBtn.destroy()
+                viewScroll.destroy()
+
+            def softResetView():
+                viewAssetBtn.config(state=ACTIVE)
+                viewWorksiteBtn.config(state=ACTIVE)
+                viewAssignmentsBtn.config(state=ACTIVE)
+
+            viewAssetBtn.config(state=DISABLED)
+            viewWorksiteBtn.config(state=DISABLED)
+            viewAssignmentsBtn.config(state=DISABLED)
+
+            assignmentList = database.viewAllAssignments(conn)
+
+            if not len(assignmentList) == 0:
+                viewScroll = Scrollbar(resultFrame)
+                assignmentTree = ttk.Treeview(resultFrame,height=20,yscrollcommand=viewScroll.set)
+                assignmentTree['columns']=("Assignment ID","Asset ID","Asset Type","Asset Name","Asset Model No",
+                                           "Worksite ID", "Order ID", "Worksite Type")
+                assignmentTree.column("#0",width=1)
+                assignmentTree.column("Assignment ID",width=120)
+                assignmentTree.column("Asset ID",width=100,anchor="center")
+                assignmentTree.column("Asset Type",width=120,anchor="center")
+                assignmentTree.column("Asset Name",width=120,anchor="center")
+                assignmentTree.column("Asset Model No",width=150,anchor="center")
+                assignmentTree.column("Worksite ID",width=100,anchor="center")
+                assignmentTree.column("Order ID",width=120,anchor="center")
+                assignmentTree.column("Worksite Type",width=150,anchor="center")
+                assignmentTree.heading("#0", text="")
+                assignmentTree.heading("Assignment ID", text="Assignment ID")
+                assignmentTree.heading("Asset ID", text="Asset ID")
+                assignmentTree.heading("Asset Type", text="Asset Type")
+                assignmentTree.heading("Asset Name", text="Asset Name")
+                assignmentTree.heading("Asset Model No", text="Asset Model No")
+                assignmentTree.heading("Worksite ID", text="Worksite ID")
+                assignmentTree.heading("Order ID", text="Order ID")
+                assignmentTree.heading("Worksite Type", text="Worksite Type")
+                assignmentTree.tag_configure('even', background="gainsboro")
+                assignmentTree.tag_configure('odd', background="white")
+                i=0
+                for assignment in range(0,len(assignmentList)):
+                    resultTup = assignmentList[assignment]
+                    resultAssetID = resultTup[1]
+                    resultWorksiteID = resultTup[2]
+                    foundAsset = database.viewOneAsset(conn, resultAssetID)
+                    foundWorksite = database.viewOneWorksite(conn, resultWorksiteID)
+                    if i % 2 == 0:
+                        assignmentTree.insert(parent="",index='end',iid=i,text="val",values=(
+                                            resultTup[0],resultTup[1],foundAsset[1],
+                                            foundAsset[2],foundAsset[5],resultTup[2],
+                                            foundWorksite[1],foundWorksite[2]),tags=('even',))
+                    else:
+                        assignmentTree.insert(parent="",index='end',iid=i,text="val",values=(
+                                            resultTup[0],resultTup[1],foundAsset[1],
+                                            foundAsset[2],foundAsset[5],resultTup[2],
+                                            foundWorksite[1],foundWorksite[2]),tags=('odd',))
+                    i+=1
+                assignmentTree.pack(side="left")
+                viewScroll.pack(side="left",fill="y")
+                viewScroll.config(command=assignmentTree.yview)
+                resetBtn = Button(resultFrame,text="Reset", font=('Arial',20),
+                                       command=lambda: resetView())
+                resetBtn.pack(side="right",padx=50)
+            else:
+                messagebox.showerror("Missing Data","No Assignments Found")
+                softResetView()
         
         ##============================================================
         ## LAYOUT SETTINGS
@@ -749,7 +1039,7 @@ class ViewPage(Frame):
         viewAssetBtn.grid(row=1,column=0,columnspan=2)
         viewWorksiteBtn = Button(contentFrame, text="View Worksites", font=('Arial',20),command=lambda: DisplayWorksites())
         viewWorksiteBtn.grid(row=1,column=3,columnspan=2)
-        viewAssignmentsBtn = Button(contentFrame, text="View Assignments", font=('Arial',20))
+        viewAssignmentsBtn = Button(contentFrame, text="View Assignments", font=('Arial',20),command=lambda: DisplayAssignments())
         viewAssignmentsBtn.grid(row=1,column=6,columnspan=2)
 
 class AssignmentPage(Frame):
